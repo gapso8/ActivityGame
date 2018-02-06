6d6b0d4c67a79e915c19bbdbf329d30703850e16
gapso8 <gapimerela123@gmail.com>
2018-02-07 00:30:26 +0100

    gradle fix02

--- app/src/main/java/com/example/gaper/activity/SecondActivity.java
+++ app/src/main/java/com/example/gaper/activity/SecondActivity.java
@@ -5,43 +5,60 @@ import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import android.content.Intent;
+import android.net.Uri;
 
 public class SecondActivity extends MainActivity {
 
     private TextView time;
-    private TextView Word;
+    private TextView share;
+    private Button word;
     private Button start;
     private Button stop;
     private Button up;
     private Button down;
     private Button newWord;
+    private Button end;
     private CountDownTimer countDownTimer;
 
     private String[] words;
+    private String[] presentation = {"speaking", "drawing", "pantomime"};
+    private String a = "";
 
     private View.OnClickListener btnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.start:
-                    enableButtons(false);
                     start(90);
+                    enableButtons(true, 4);
                     break;
                 case R.id.stop:
                     cancel();
-                    enableButtons(true);
+                    enableButtons(true, 2);
                     break;
                 case R.id.newWord:
                     setRandomWord();
+                    enableButtons(false, 1);
+                    break;
+                case R.id.up:
+                    enableButtons(true, 3);
+                    break;
+                case R.id.down:
+                    enableButtons(true, 3);
+                    break;
+                case R.id.word:
+                    goTo(v);
+                    break;
+                case R.id.end:
+                    theEnd();
                     break;
             }
         }
     };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,17 +70,25 @@ public class SecondActivity extends MainActivity {
         stop.setOnClickListener(btnClickListener);
         time = (TextView) findViewById(R.id.time);
         up =  (Button)findViewById(R.id.up);
+        up.setOnClickListener(btnClickListener);
         down = (Button)findViewById(R.id.down);
+        down.setOnClickListener(btnClickListener);
         newWord = (Button)findViewById(R.id.newWord);
         newWord.setOnClickListener(btnClickListener);
-        Word = (TextView) findViewById(R.id.Word);
+        word = (Button) findViewById(R.id.word);
+        word.setOnClickListener(btnClickListener);
+        share = (TextView) findViewById(R.id.share);
+        end = (Button) findViewById(R.id.end);
+        end.setOnClickListener(btnClickListener);
 
         createWordsList();
-        setRandomWord();
+        //setRandomWord();
+        word.setText(null);
+        share.setText(null);
     }
 
     public void start(int timeS) {
-        time.setText("15");
+        time.setText(null);
 
         countDownTimer = new CountDownTimer(timeS * 1000, 1000) {
             @Override
@@ -77,12 +102,11 @@ public class SecondActivity extends MainActivity {
             }
             @Override
             public void onFinish() {
-                time.setText("Done!!!");
+                time.setText("Time's up!");
             }
         };
 
         countDownTimer.start();
-
     }
 
     public void cancel() {
@@ -93,14 +117,37 @@ public class SecondActivity extends MainActivity {
         }
     }
 
-    private void enableButtons(boolean enable){
+    private void enableButtons(boolean enable, int a){
         if (enable) {
-            up.setEnabled(true);
-            down.setEnabled(true);
+            switch (a) {
+                case 1:
+                    up.setEnabled(true);
+                    down.setEnabled(true);
+                    newWord.setEnabled(true);
+                    break;
+                case 2:
+                    up.setEnabled(true);
+                    down.setEnabled(true);
+                    stop.setEnabled(false);
+                    break;
+                case 3:
+                    newWord.setEnabled(true);
+                    up.setEnabled(false);
+                    down.setEnabled(false);
+                    break;
+                case 4:
+                    start.setEnabled(false);
+                    stop.setEnabled(true);
+                    word.setEnabled(false);
+                    break;
+            }
         }
         else {
             up.setEnabled(false);
             down.setEnabled(false);
+            newWord.setEnabled(false);
+            start.setEnabled(true);
+            word.setEnabled(true);
         }
     }
 
@@ -115,6 +162,23 @@ public class SecondActivity extends MainActivity {
     }
 
     private void setRandomWord () {
-        Word.setText(words[(int)(Math.random() * 998) + 1]);
+        a = words[(int)(Math.random() * words.length-1)];
+        word.setText(a);
+        share.setText(presentation[(int)(Math.random() * presentation.length)]);
+    }
+
+    public void goTo (View view) {
+        goToUrl ( "http://www.dictionary.com/browse/"+ a +"?s=t");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    private void theEnd (){
+        Intent j = new Intent(SecondActivity.this, EndActivity.class);
+        startActivity(j);
     }
 }
